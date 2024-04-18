b4b9b2a1b143be45099fdcc6d05842a042beaa28
Escobar28 <eq17001@ues.edu.sv>
2024-04-18 13:20:42 -0600

    SQL alter table column habilitado

---
+++ HabilitadoUpdate.sql
@@ -0,0 +1,15 @@
+alter table areaunidadorgani add habilitado char(1) default '1';
+alter table depunidadorgani add habilitado char(1) default '1';
+alter table descuento add habilitado char(1) default '1';
+alter table documento add habilitado char(1) default '1';
+alter table empleado add habilitado char(1) default '1';
+alter table empresa add habilitado char(1) default '1';
+alter table ingreso add habilitado char(1) default '1';
+alter table profesion add habilitado char(1) default '1';
+alter table puesto add habilitado char(1) default '1';
+alter table seccionesunidadorgani add habilitado char(1) default '1';
+alter table subseccionesunidadorgani add habilitado char(1) default '1';
+alter table tipodescuento add habilitado char(1) default '1';
+alter table tipodocumento add habilitado char(1) default '1';
+alter table tipoingreso add habilitado char(1) default '1';
+alter table unidadorganizativa add habilitado char(1) default '1';
